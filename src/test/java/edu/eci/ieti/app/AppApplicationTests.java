f0e3fe94ce2cdc79ac8cb63a1b25a9fd3b7c0436
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-01-26 11:32:22 -0500

    adding test

--- src/test/java/edu/eci/ieti/app/AppApplicationTests.java
+++ src/test/java/edu/eci/ieti/app/AppApplicationTests.java
@@ -1,13 +1,36 @@
 package edu.eci.ieti.app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class AppApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+    @SpringBootTest
+    @AutoConfigureMockMvc
+    public class HelloControllerTest {
 
+
+        //MockMvc comes from Spring Test and lets you, through a set of convenient builder classes,
+        //send HTTP requests into the DispatcherServlet and make assertions about the result.
+        //Note the use of @AutoConfigureMockMvc and @SpringBootTest to inject a MockMvc instance. Having used @SpringBootTest, we are asking for the whole application context to be created. An alternative would be to ask Spring Boot to create only the web layers of the context by using @WebMvcTest. In either case, Spring Boot automatically tries to locate the main application class of your application, but you can override it or narrow it down if you want to build something different.
+        @Autowired
+        private MockMvc mvc;
+
+        @Test
+        public void getHello() throws Exception {
+            mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+        }
+    }
 }
