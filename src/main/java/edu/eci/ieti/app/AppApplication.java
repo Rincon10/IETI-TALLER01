9445d6bedcd493d2add6eae59bdb949538b9f825
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-01-26 11:25:33 -0500

    Reading beans

--- src/main/java/edu/eci/ieti/app/AppApplication.java
+++ src/main/java/edu/eci/ieti/app/AppApplication.java
@@ -1,12 +1,16 @@
 package edu.eci.ieti.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
-
 @SpringBootApplication
+
 public class AppApplication {
 	/**
 	 * Main method
@@ -27,5 +31,20 @@ public class AppApplication {
 			return Integer.parseInt(System.getenv("PORT"));
 		}
 		return 8080;
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
 	}
 }
