36ec777a86a08c896888e24abca86b59c97e2b22
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-01-26 10:49:17 -0500

    adding dependencies

--- src/main/java/edu/eci/ieti/app/AppApplication.java
+++ src/main/java/edu/eci/ieti/app/AppApplication.java
@@ -3,11 +3,29 @@ package edu.eci.ieti.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.util.Collections;
+
 @SpringBootApplication
 public class AppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+	/**
+	 * Main method
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		SpringApplication app = new SpringApplication(AppApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+		app.run(args);
 	}
 
+	/**
+	 * Method that return the port to use in spring-boot
+	 * @return int, that represents PORT number to use in our local-app
+	 */
+	static int getPort() {
+		if (System.getenv("PORT") != null) {
+			return Integer.parseInt(System.getenv("PORT"));
+		}
+		return 8080;
+	}
 }
